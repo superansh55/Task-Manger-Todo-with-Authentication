46ebbdb7b5d1f147e9506896cc0f013a6e150bb2
superansh55 <superansh55@gmail.com>
2026-03-07 12:20:09 +0530

    fixed missing id field in task model schema

--- backend/model/TaskModel.js
+++ backend/model/TaskModel.js
@@ -14,6 +14,10 @@ const taskSchema= new Schema(
          date:{
             type:Date,
             required:true
+        },
+        user_id:{
+            type:String,
+            required:true
         }
     },{timestamps:true}
 )
