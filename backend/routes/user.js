b50ef12811a466fdec3ee810e661e565fb16d0e6
superansh55 <superansh55@gmail.com>
2026-05-04 18:01:07 +0530

    pipeline activation commit

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,7 @@
 import express from 'express'
 const router=express.Router()
 import { loginUser,signupUser } from '../controller/userController.js'
-
+//user routes
 router.post('/login',loginUser)
 router.post('/signup',signupUser)
 
